f7c5fe600a47a4db568979b212317cf50ec388c9
ManiacOfGitHub <06crhenry@gmail.com>
2024-11-04 22:02:03 -0500

    Whoops 2.0

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 async function copyScript() {
     try {
         var scriptText = await (await fetch("MSET9-ChromeOS.js")).text();
-        copy(scriptText);
+        navigator.clipboard.writeText(scriptText);
         clearTimeout(successAlertTimeout);
         document.getElementById("copied").style.display = "block";
         successAlertTimeout = setTimeout(()=>{
             document.getElementById("copied").style.display = "none";
         },5000);
     } catch(err) {
+        console.error(err);
+        clearTimeout(failedAlertTimeout);
         document.getElementById("failed").style.display = "block";
         failedAlertTimeout = setTimeout(()=>{
             document.getElementById("failed").style.display = "none";
